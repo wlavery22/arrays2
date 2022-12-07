52d844ee475f275b2118c9b9f8614f2622b22d00
William Lavery <lavery_william@yahoo.com>
2022-12-07 17:01:52 -0500

    Explain index positions

--- arrays.js
+++ arrays.js
@@ -10,3 +10,16 @@ ages.pop();
 
 // This removes the 1st element, true, from the array and returns it.
 awake.shift();
+
+/*
+The elements in an array are referred to by their order in the list,
+starting with 0, and counting 1, 2, 3...etc. This number, which describes
+the location of the element within the list is the element's index position.
+You can use this number to call elements from an array, as shown below.
+*/
+var friends = ["Tom", "Tim", "Ted"];
+console.log(friends[0]);
+/* 
+The above code should call the 1st element in the array, "Tom". The index
+position of "Tom", the 1st element, is "0".
+*/
