6859c27d90dbf1e8a9bfaa1d56ca191218ead041
William Lavery <lavery_william@yahoo.com>
2022-12-07 16:47:06 -0500

    Add 3 array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var cats = ["Amy", "Ann", "Al", "Tim"];
 var ages = [5, 10, 15, 20];
 var awake = [true, false, true, false];
+
+// This will call the 1st element in the array, it outputs "Amy"
+console.log(cats[0]);
+
+// This removes the last element, 20, from the array and returns it.
+ages.pop();
+
+// This removes the 1st element, true, from the array and returns it.
+awake.shift();
